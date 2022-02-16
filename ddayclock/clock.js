2e161a49c316d145b8a867b81d9cbbb0f889b163
yeeed711 <rladpwl910@naver.com>
2022-02-16 13:42:58 +0900

    clock

---
+++ ddayclock/clock.js
@@ -0,0 +1,21 @@
+const clock = document.querySelector(".clock");
+
+function dDay() {
+    const today = new Date().getTime();
+    const christmas = new Date(2022, 11, 25).getTime();
+    const leftDay = (christmas - today)/1000;
+    //24hours, 60minutes, 60seconds, 1000milliseconds
+    const day = Math.floor(leftDay/(60*60*24));
+    const hour = Math.floor(leftDay/(60*60))%24;
+    const minute = Math.floor(leftDay/60) % 60; 
+    const second = Math.floor(leftDay) % 60;
+
+    const dDayText = `${day}D ${hour}h ${minute}m ${second}s`;
+    
+    clock.innerText = dDayText;
+
+
+}
+
+dDay();
+setInterval(dDay, 1000);
